98c4ec981de4b558abc1903b6e1a9798605419b9
Amit Yadav <yadavamit006@gmail.com>
2024-12-05 10:07:07 +0530

    Java change

--- src/main/java/com/sample/microservices/currencyexchange/CircuitBrakerController.java
+++ src/main/java/com/sample/microservices/currencyexchange/CircuitBrakerController.java
@@ -1,7 +1,12 @@
 package com.sample.microservices.currencyexchange;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +23,9 @@ public class CircuitBrakerController {
 
 	@GetMapping("/sample-api")
 	//@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
-	//@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
-	//@RateLimiter(name="default")
-	@Bulkhead(name="sample-api")
+	//@CircuitBreaker(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	//@RateLimiter(name="default",fallbackMethod = "rateLimitingFallback")
+	//@Bulkhead(name="sample-api")
 	//10s => 10000 calls to the sample api
 	public String sampleApi() {
 		logger.info("Sample api call received");
@@ -28,6 +33,10 @@ public class CircuitBrakerController {
 //					String.class);
 //		return forEntity.getBody();
 		return "sample-api";
+	}
+
+	public String rateLimitingFallback(Throwable d) {
+		return "Too Many Requests - Retry After 1 Minute";
 	}
 	
 	public String hardcodedResponse(Exception exception) {
